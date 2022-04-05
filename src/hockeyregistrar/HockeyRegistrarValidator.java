4cd46f6a84d90c962351811196230bdb30e25dab
ProfPaulB <paulbonenfant@hotmail.com>
2022-04-05 15:18:43 -0400

    Added 9 or over test

--- src/hockeyregistrar/HockeyRegistrarValidator.java
+++ src/hockeyregistrar/HockeyRegistrarValidator.java
@@ -7,17 +7,17 @@ import java.time.LocalDate;
  * for an 'under 10' team. Used for final exam review.
  *
  * @author dancye, 2019
- * @author Paul Bonenfant April 2021
+ * @author Paul Bonenfant April 2022
  */
 public class HockeyRegistrarValidator {
 
     /**
      * A method that validates that players are eligible for a
      * '10 and under' hockey club. In order to qualify, they must
-     * be 10 or less by Dec 31 of the current year
+     * be over 9 but less than 10 by Dec 31 of the current year
      *
      * @param playerBirthYear, the player's year of birth
-     * @return true if the player will be 10 or under 
+     * @return true if the player will be over 9 but less than 10 
      * this year, false otherwise.
      */
     public boolean validateBirthYear(int playerBirthYear) {
@@ -26,9 +26,8 @@ public class HockeyRegistrarValidator {
         LocalDate date = LocalDate.now();
         int year = date.getYear();    
         
-        if (playerBirthYear >= (year - 10)) {
-            return true;
-        }
-        return false;
+        int playerAge = year - playerBirthYear;
+        
+        return playerAge >= 9 && playerAge < 10;
     }
 }
